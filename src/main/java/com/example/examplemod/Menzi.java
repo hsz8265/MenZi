e0cbe5bdcf4beaaf70f175a8bfd807471bd206ed
hsz <hushizhih@gmail.com>
2025-07-04 11:15:43 +0800

    aaa

--- src/main/java/com/example/examplemod/Menzi.java
+++ src/main/java/com/example/examplemod/Menzi.java
@@ -29,8 +29,8 @@ import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(ExampleMod.MODID)
-public class ExampleMod
+@Mod(Menzi.MODID)
+public class Menzi
 {
     // Define mod id in a common place for everything to reference
     public static final String MODID = "Menzi";
@@ -60,7 +60,7 @@ public class ExampleMod
                 output.accept(EXAMPLE_ITEM.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
             }).build());
 
-    public ExampleMod(FMLJavaModLoadingContext context)
+    public Menzi(FMLJavaModLoadingContext context)
     {
         IEventBus modEventBus = context.getModEventBus();
 
